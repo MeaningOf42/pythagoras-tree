63ef1c4adee3dd46efe44671ab43041687b2c130
MeaningOf42 <biltimi@gmail.com>
2018-02-27 13:51:11 +0100

    Added constructor and show to square class.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
 class Square {
-  
+  constructor(l, x,y) {
+    this.l = l;
+    this.x = x;
+    this.y = y;
+  }
+
+  show(color) {
+    var point1 = {x  , y  };
+    var point2 = {x  , y+l};
+    var point3 = {x+l, y+l};
+    var point4 = {x  , y  };
+
+    fill(color);
+    beginShape();
+    vertex(point1);
+    vertex(point2);
+    vertex(point3);
+    vertex(point4);
+    endShape();
+  }
+
 }
+
+let testSquare;
 
 function setup() {
   createCanvas(620, 620);
+  testSquare = Square(50, 0,0);
 }
 
 function draw() {
-  background(0);
+  background(200);
+  testSquare.show(0);
 }
